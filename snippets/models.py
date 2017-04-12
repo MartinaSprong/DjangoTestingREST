b9721f1ee954f5f9b1197855a7bc49d5d1c5589d
Martina Sprong <martinasprong@hotmail.com>
2017-04-12 15:00:50 +0200

    Tide parameter added

--- snippets/models.py
+++ snippets/models.py
@@ -9,6 +9,15 @@ STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 # time and location per parameter
 # geometry field for geodjango        
 
+class tide(models.Model):
+    parameterName = models.CharField(max_length=50)
+    value = models.IntegerField()
+    unit = models.CharField(max_length=50)
+    time = models.DateTimeField(default=1)
+    locationName = models.CharField(max_length=50)
+    lat = models.FloatField()
+    lon = models.FloatField() 
+
 class chlorosity(models.Model):
     categoryName = models.CharField(max_length=50)
     parameterName = models.CharField(max_length=50)
